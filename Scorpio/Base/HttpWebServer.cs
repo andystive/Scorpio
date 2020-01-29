09974cbcb76ef75ce3ec1a4a37aef094e6788600
root <root@Anonymous.localdomain>
2020-01-29 13:41:32 +0800

    2020-01-29 by Virus

--- Scorpio/Base/HttpWebServer.cs
+++ Scorpio/Base/HttpWebServer.cs
@@ -1,12 +1,36 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Net;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace Scorpio.Base
 {
-    class HttpWebServer
+    public class HttpWebServer
     {
+        private HttpListener _listener;
+        private Func<string, string> _responderMethod;
+
+        public HttpWebServer(string[] prefixes, Func<string, string> method)
+        {
+            try
+            {
+                _listener = new HttpListener();
+
+                if (!HttpListener.IsSupported)
+                    throw new NotSupportedException(
+                    "Needs Windows XP SP2, Server 2003 or later.");
+
+                // URL prefixes are required, for example
+                // "http://localhost:8080/index"
+                if (prefixes == null || prefixes.Length == 0)
+                    throw new ArgumentException("prefixes");
+
+                // A reponder method is required
+                if (method == null)
+                    throw new ArgumentException("method");
+
+                foreach (string s in prefixes)
+
+            }
+        }
     }
-}
