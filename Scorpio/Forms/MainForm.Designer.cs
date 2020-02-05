460381ce7623606cafd232e21518387042a5d077
Jobs <vvirus@foxmail.com>
2020-02-05 16:20:09 +0800

    2020-02-05 更新窗体

--- Scorpio/Forms/MainForm.Designer.cs
+++ Scorpio/Forms/MainForm.Designer.cs
@@ -32,6 +32,53 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this.splitContainerl = new System.Windows.Forms.SplitContainer();
             this.lvServers = new Scorpio.Base.ListViewFlickerFree();
+            this.cmsLv = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.toolStripSeparatorl = new System.Windows.Forms.ToolStripSeparator();
+            this.menuSetDefaultServer = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator3 = new System.Windows.Forms.ToolStripSeparator();
+            this.menuPingServer = new System.Windows.Forms.ToolStripMenuItem();
+
+            this.tsbServer = new System.Windows.Forms.ToolStripDropDownButton();
+            this.notifyMain = new System.Windows.Forms.NotifyIcon(this.components);
+            this.cmsMain = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.menuSysAgentMode = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuNotEnabledHttp = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuGlobal = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuGlobalPAC = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuKeep = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuKeepPAC = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuServers = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.menuExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txtMsgBox = new System.Windows.Forms.TextBox();
+           
+            this.ssMain = new System.Windows.Forms.StatusStrip();
+            this.toolSslSocksPortLab = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolSslSocksPort = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolSslBlank1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolSslHttpPortLab = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolSslHttpPort = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolSslBlank2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolSslPacPortLab = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolSslPacPort = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolSslBlank3 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolSslServerSpeed = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolSslBlank4 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.panell = new System.Windows.Forms.Panel();
+            this.tsMain = new System.Windows.Forms.ToolStrip();
+            this.toolStripSeparator4 = new System.Windows.Forms.ToolStripSeparator();
+            this.tsbSub = new System.Windows.Forms.ToolStripDropDownButton();
+            this.tsbSubSetting = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsSubUpdate = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator8 = new System.Windows.Forms.ToolStripSeparator();
+            this.tsbOptionSetting = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator5 = new System.Windows.Forms.ToolStripSeparator();
+            this.tsbReload = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator7 = new System.Windows.Forms.ToolStripSeparator();
+            this.tsbCheckUpdate = new System.Windows.Forms.ToolStripDropDownButton();
+            this.tsbCheckUpdatePACList = new System.Windows.Forms.ToolStripMenuItem();
 
 
             this.SuspendLayout();
